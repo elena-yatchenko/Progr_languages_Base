4d90cf250ca662e23c430db0ba83003c2a27fbff
Elena <hel.lena84@mail.ru>
2023-09-10 17:33:04 +0400

    ex_4_divide

--- Lect_1_Ex_3/Program.cs
+++ Lect_1_Ex_3/Program.cs
@@ -1,7 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int numberA = 31;
-int numberB = 5;
-int result = numberA + numberB;
+// int numberA = 31;
+// int numberB = 5;
+// int result = numberA + numberB;
 
-// Console.WriteLine(numberA + numberB);
-Console.WriteLine(result);
+// // Console.WriteLine(numberA + numberB);
+// Console.WriteLine(result);
+
+// int numberA = 10;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB); // 2
+
+// если сделаем 12 / 5, то тоже получим 2, т.к. используем деление в целых числах. int(integer) - тип данных(контейнер), 
+// позволяющий работать с целыми числами
+
+// int numberA = 12;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB); // 2
+
+// исправляем, заменяя int на double (контейнер, позволяющий работать с вещественными (дробными) числами)
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB); // 2
