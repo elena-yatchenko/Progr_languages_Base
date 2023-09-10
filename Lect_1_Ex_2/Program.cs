e9960f9cf5eb49f064b7bf8ec4851d14bfef9d9c
Elena <hel.lena84@mail.ru>
2023-09-10 16:27:43 +0400

    example_2

---
+++ Lect_1_Ex_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Введите ваше имя ");
+// string username = Console.ReadLine();
+// // в программировании '=' - оператор присваивания, который говорит: 
+// // "возьми то, что справа, и положи в то, что слева"
+// Console.WriteLine("Привет, "); // пишет все на отдельной строке
+// Console.WriteLine(username);
+
+// Введите ваше имя 
+// Elena
+// Привет, 
+// Elena
+
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+
+// Введите ваше имя Елена
+// Привет, Елена
+
