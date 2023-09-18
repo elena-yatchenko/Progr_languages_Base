404a676c543e1d3603843e03e7bd2443b561859a
Elena <hel.lena84@mail.ru>
2023-09-18 13:33:55 +0400

    Lect_3

---
+++ Lect_3/Lect_3_Ex_1_Methods/Program.cs
@@ -0,0 +1,67 @@
+﻿// 4 группы методов: 
+// 1) ничего не принимают, ничего не возвращают
+// 2) что-то принимают, ничего не возвращают
+// 3) ничего не принимают, но могут возвращать что-то (например, генерировать случайные элементы)
+// 4) что-то принимают, что-то возвращают
+
+// 1 и 2 - void-методы
+
+// Group 1
+void Method1()
+{
+    Console.WriteLine("Автор... ");
+}
+// Method1(); // - вызов метода
+
+// Group 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Method2("Текст сообщения") // Вызов метода;
+
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i += 1;
+    }  
+}
+// Когда аргументов несколько, лучше использовать именованные аргументы: 
+// т.е. явно указывать какому аргументу какое значение присваиваем. В этом случае их можно вводить не по порядку
+
+// Method21(msg: "Text of message", count: 4);
+
+// CTRL + C - прерывание, если программа зациклилась (т.е. в бесконечный цикл вошла)
+
+// Group 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+// Method3(); // вызов
+
+int year = Method3();
+// Console.WriteLine(year);
+
+// Group 4
+string Method4(int count, string text)
+{
+    int i = 0; 
+    string result = ""; // = string.Empty
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "testing");
+Console.WriteLine(res);
